143e445291996edbed358c814f1e0c18ccfe2499
Mauran Muthiah <mauran.muthiah-cic@ibm.com>
2023-03-11 01:05:23 +0100

    Added links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,41 @@
 import './globals.css'
 
 export default function Home() {
+
+  const streamingServices = [
+    {
+      'title': 'DR',
+      'link': 'http://dr.dk/tv'
+    }, 
+    {
+      'title': 'Viaplay',
+      'link': 'https://viaplay.dk'
+    },
+    {
+      'title': 'HBO Max',
+      'link': 'https://www.hbomax.com'
+    },
+    {
+      'title': 'Cmore',
+      'link': 'https://cmore.dk'
+    },
+    {
+      'title': 'Discovery+',
+      'link': 'https://www.discoveryplus.com/dk/'
+    },
+    {
+      'title': 'TV2 Play',
+      'link': 'https://play.tv2.dk/'
+    },
+  ];
+
   return (
     <main className="bg-black">
-     Hello world
+      <div className='bg-black'>
+        Hello
+      </div>
+     { streamingServices.map(obj => <><li><a href={obj.link}>{obj.title}</a></li></>)}
+     <a href="https://www.youtube.com/redirect?q=https://biograf.vercel.app/">Gå fullscreen</a>
     </main>
   )
 }
